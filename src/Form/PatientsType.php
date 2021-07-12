41b25cfb30437c41b7d85498107e3c5959bf64aa
crisalejos <cristina-alejos@outlook.com>
2021-07-12 11:43:33 +0200

    mise en forme

--- src/Form/PatientsType.php
+++ src/Form/PatientsType.php
@@ -6,17 +6,50 @@ use App\Entity\Patients;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 class PatientsType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+       
+        
         $builder
-            ->add('lastname')
-            ->add('firstname')
-            ->add('birthDate')
-            ->add('email')
-            ->add('vitalcardNumber')
+            ->add('lastname',  TextType::class, [
+                'label' => 'Nom : ',
+                'label_attr'=>['class' => 'form-label'],
+                'attr'=>['class' => 'form-control']
+                
+            ])
+            ->add('firstname',  TextType::class, [
+                'label' => 'Prénom : ',
+                'label_attr'=>['class' => 'form-label'],
+                'attr'=>['class' => 'form-control']
+                
+            ])
+            ->add('birthDate',  DateType::class, [
+                'label' => 'Date de naissance : ',
+                'label_attr'=>['class' => 'form-label'],
+                'attr'=>['class' => 'mt-3 mb-3 combinedPickerInput'],
+                'placeholder' => [
+                    'year' => 'Année', 'month' => 'Mois', 'day' => 'Jour',
+                ]
+                
+            ])
+            ->add('email',  EmailType::class, [
+                'label' => 'Email : ',
+                'label_attr'=>['class' => 'form-label'],
+                'attr'=>['class' => 'form-control']
+                
+            ])
+            ->add('vitalcardNumber',  NumberType::class, [
+                'label' => 'Numéro de carte : ',
+                'label_attr'=>['class' => 'form-label'],
+                'attr'=>['class' => 'form-control']
+                
+            ])
         ;
     }
 
